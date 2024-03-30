4c519dc40e5fbfa2bf5bde36454ef5558564d6c2
Muhammad Adil Mehmood <iamadilmalick@gmail.com>
2024-03-31 02:20:24 +0500

    Initial Commit

---
+++ i-am-rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i-am-rich
+//
+//  Created by Muhammad Adil Mehmood on 31/03/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
